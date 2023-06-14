73d0b6149c6e7eeb5f7f116bdc714dd71fd04ec4
JobsonOliveira <jobsonoliveira007@gmail.com>
2023-06-13 22:04:43 -0300

    Calculos otimizados, soma, subtração, divisão e multiplicação concluídos

--- codigo.js
+++ codigo.js
@@ -1,8 +1,8 @@
-			let NumAtual = [];
-			let NumAnterior = [];
-			NumAtual = document.querySelector("#caractere_atual");
-			NumAnterior = document.querySelector("#caractere_anterio");
+			let NumAtual = document.querySelector("#caractere_atual");
+			let NumAnterior = document.querySelector("#caractere_anterio");
 			let arrayNumAtual = []
+			let arrayNumAnterior = []
+			let arrayTodaOperacao = []
 			let operacao = ""
 			
 			/*--ADICIONAR OS CARACTERES NA TELA--*/
@@ -10,19 +10,33 @@
 				//VERIFICA SE JÁ TEM UM PONTO NO NUMERO DIGITADO E SE JÁ FOI DIGITADO ALGUM NÚMERO ANTES DELE
 				if(((caractere == '.') && (arrayNumAtual.length == 0)) || ((caractere == '.') && (arrayNumAtual.includes('.')))){
 					return;
-				}else{
-					if((NumAnterior != '') && (NumAnterior.includes(operacao))){
-						alert(NumAtual);
-						arrayNumAtual += caractere;
-						NumAtual.innerText = arrayNumAtual;						
-					}else{
-						alert(NumAtual);
-						NumAnterior.innerText += ` ${operacao}`;	
-						arrayNumAtual += caractere;
-						NumAtual.innerText = arrayNumAtual;
-					}			
 				}
+				//ADICIONAR OS CARACTERES
+				else{
+						//QUALDO TIVER SIDO DIGITADO UM OPERADOR
+						if((operacao != '+') && (operacao != '-') && (operacao != '*') && (operacao != '/')){
+
+							arrayNumAtual += caractere;
+							NumAtual.innerText = arrayNumAtual;
+							arrayTodaOperacao = arrayNumAtual;
+						}
+						//QUANDO NÃO TIVER SIDO DIGITADO UM OPERDOR
+						else{
+							arrayTodaOperacao += ` ${caractere}`
+
+							if(arrayTodaOperacao.includes(operacao)){
+								arrayNumAtual += `${caractere}`;
+								NumAtual.innerText = arrayNumAtual;
+							}else{
+								NumAnterior.innerText += ` ${operacao}`;
+								arrayNumAtual = caractere;
+								NumAtual.innerText = arrayNumAtual;
+								arrayTodaOperacao += ` ${operacao} ${caractere}`;
+							}
+						}
+					}			
 			}
+			
 			
 			//ADICIONAR OS OPERADORES
 			function addOperacao(operador){
@@ -32,14 +46,66 @@
 				}
 				//SE FOI DIGITADO UM NÚMERO A NÃO DIGITADO O OPERADOR
 				else{
-					NumAnterior.innerText = arrayNumAtual;
+					arrayNumAnterior = arrayNumAtual;
+					arrayNumAtual = "";
+					NumAnterior.innerText = arrayNumAnterior;
 					NumAtual.innerText = operador;
-					arrayNumAtual = ""
 					operacao = operador;
 				}
 			}
 			
 			//APAGAR O ULTIMO CARACTERE		
-			let btnApagar = document.querySelector("#deletar").addEventListener('click', () => {
-				alert('js');
+			let btnApagar = document.querySelector("#deletar");
+			btnApagar.addEventListener('click', () => {
+				
+			});
+
+			//APRESENTAR OS RESULTADOS
+			let btnSoma = document.querySelector('#somar');
+			btnSoma.addEventListener('click', () => {
+				var numero1 = parseFloat(arrayNumAnterior);
+				var numero2 = parseFloat(arrayNumAtual);
+				
+
+				switch (operacao) {
+					case '+':
+						NumAnterior.innerText = "";
+						arrayNumAnterior = "";
+						arrayNumAtual = "";
+						operacao = '';
+						NumAtual.innerText = numero1 + numero2;
+						numero1 = '';
+						numero2 = '';
+						break;
+					case '-':
+						
+						NumAnterior.innerText = "";
+						arrayNumAnterior = "";
+						arrayNumAtual = "";
+						operacao = '';
+						NumAtual.innerText = numero1 - numero2;
+						numero1 = '';
+						numero2 = '';
+						break;
+					case '*':
+						NumAnterior.innerText = "";
+						arrayNumAnterior = "";
+						arrayNumAtual = "";
+						operacao = '';
+						NumAtual.innerText = numero1 * numero2;
+						numero1 = '';
+						numero2 = '';
+						break;
+					case '/':
+						NumAnterior.innerText = "";
+						arrayNumAnterior = "";
+						arrayNumAtual = "";
+						operacao = '';
+						NumAtual.innerText = numero1 / numero2;
+						numero1 = '';
+						numero2 = '';
+						break;		
+					default:
+						break;
+				}
 			});
