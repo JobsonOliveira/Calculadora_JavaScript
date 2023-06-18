f51fa683e59a38ee8bb347ec52a02a6c3cad88e2
JobsonOliveira <jobsonoliveira007@gmail.com>
2023-06-17 21:12:51 -0300

    Calculadora concluida

--- codigo.js
+++ codigo.js
@@ -7,39 +7,57 @@ let operacao = ""
 
 /*--ADICIONAR OS CARACTERES NA TELA--*/
 function addCaractere(caractere){
-	//VERIFICA SE JÁ TEM UM PONTO NO NUMERO DIGITADO E SE JÁ FOI DIGITADO ALGUM NÚMERO ANTES DELE
-	if(((caractere == '.') && (arrayNumAtual.length == 0)) || ((caractere == '.') && (arrayNumAtual.includes('.')))){
+	//VERIFICA SE JÁ TEM UM PONTO NO NUMERO DIGITADO E SE JÁ FOI DIGITADO ALGUM NÚMERO ANTES DELE (SEGUNDO NÚMERO)
+	if((caractere == '.') && (NumAtual.innerText != '') && (NumAnterior.innerText != '')){
+		if((NumAtual.innerText != '+') && (NumAtual.innerText != '-') && (NumAtual.innerText != '*') && (NumAtual.innerText != '/')){
+
+			arrayNumAtual += `${caractere}`;
+			NumAtual.innerText = arrayNumAtual;
+			arrayTodaOperacao += arrayNumAtual;
+		}
+		
+		if(arrayNumAtual.includes(".") == true){
+			NumAtual.innerText = NumAtual.innerText.slice(0, -1);
+			arrayNumAtual = NumAtual.innerText;
+		}
+
+	}
+	//VERIFICA SE JÁ TEM UM PONTO NO NUMERO DIGITADO E SE JÁ FOI DIGITADO ALGUM NÚMERO ANTES DELE (PRIMEIRO NÚMERO)
+	if(caractere == '.' && ((NumAtual.innerText == '') || (arrayNumAtual.includes('.')))){
+		
 		return;
-				}
+	}
 	//ADICIONAR OS CARACTERES
 	else{
-			//QUALDO NÃO TIVER SIDO DIGITADO UM OPERADOR
-			if((operacao != '+') && (operacao != '-') && (operacao != '*') && (operacao != '/')){
-				arrayNumAtual += caractere;
-				NumAtual.innerText = arrayNumAtual;
-				arrayTodaOperacao += arrayNumAtual;
-			}
-			//QUANDO NÃO TIVER SIDO DIGITADO UM OPERDOR (FAZ O OPERADOR SUBIR)
-			else{
-				if(arrayTodaOperacao.includes(operacao)){
-					arrayNumAtual += `${caractere}`;
-					NumAtual.innerText = arrayNumAtual;
-				}else{
-					NumAnterior.innerText += ` ${operacao}`;
-					arrayNumAtual = caractere;
-					NumAtual.innerText = arrayNumAtual;
-					arrayTodaOperacao += ` ${operacao} ${caractere}`;
-				}
-			}
-		}			
-}
+		//QUALDO NÃO TIVER SIDO DIGITADO UM OPERADOR
+		if((NumAtual.innerText != '+') && (NumAtual.innerText != '-') && (NumAtual.innerText !='*') && (NumAtual.innerText != '/')){
+			arrayNumAtual += `${caractere}`;
+			NumAtual.innerText = arrayNumAtual;
+			arrayTodaOperacao += arrayNumAtual;
+		}
+		//QUANDO TIVER SIDO DIGITADO UM OPERDOR (FAZ O OPERADOR SUBIR)
+		else{
+			if((caractere != '.') && ((NumAtual.innerText == '+') || (NumAtual.innerText == '-') || (NumAtual.innerText == '*') || (NumAtual.innerText == '/'))){
 
+				NumAnterior.innerText += ` ${operacao}`;
+				arrayNumAtual = caractere;
+				NumAtual.innerText = arrayNumAtual;
+				arrayTodaOperacao += ` ${operacao} ${caractere}`;
+			}
+		}
+	}
+}
 
 //ADICIONAR OS OPERADORES
 function addOperacao(operador){
 	//SE O NENHU NÚMERO FOI DIGITADO OU SE JÁ FOI DIGITADO O MESMO OPERADOR
-	if((arrayNumAtual == "") || (operacao.includes('+')) || (operacao.includes('-')) || (operacao.includes('*')) || (operacao.includes('/'))){
+	if((arrayNumAtual.innerText == "") || (arrayNumAtual.innerText == '+') || (arrayNumAtual.innerText == '-') || (arrayNumAtual.innerText == '*') || (arrayNumAtual.innerText == '/')){
 		return;
+	}
+	//MUDANDO O OPERADOR
+	else if(arrayNumAnterior != ""){
+		operacao = operador;
+		NumAtual.innerText = operacao;
 	}
 	//SE FOI DIGITADO UM NÚMERO A NÃO DIGITADO O OPERADOR
 	else{
@@ -54,9 +72,14 @@ function addOperacao(operador){
 //APAGAR O ULTIMO CARACTERE		
 let btnApagar = document.querySelector("#deletar");
 btnApagar.addEventListener('click', () => {
-	//SE FOR PARA APAGAE O OPERADOR
-	arrayNumAtual = arrayNumAtual.slice(0, -1);
-	NumAtual.innerText = NumAtual.innerText.slice(0, -1);;
+	if((NumAtual.innerText == '+') || (NumAtual.innerText == '-') || (NumAtual.innerText == '*') || (NumAtual.innerText == '/')){
+
+		NumAtual.innerText = '';
+		operacao = '';
+	}else{
+		NumAtual.innerText = NumAtual.innerText.slice(0, -1);
+		arrayNumAtual = NumAtual.innerText;
+	}
 });
 
 //APAGAR TUDO
@@ -69,55 +92,61 @@ btnDelAll.addEventListener('click', () =>{
 	NumAnterior.innerText = ''
 	arrayTodaOperacao = ''
 });
+
 //APRESENTAR OS RESULTADOS
 let btnSoma = document.querySelector('#somar');
 btnSoma.addEventListener('click', () => {
 	var numero1 = parseFloat(arrayNumAnterior);
 	var numero2 = parseFloat(arrayNumAtual);
 	
-	switch (operacao) {
-		case '+':
-			NumAnterior.innerText = ""
-			arrayNumAnterior = ""
-			arrayNumAtual = numero1 + numero2;
-			NumAtual.innerText = arrayNumAtual;
-			arrayTodaOperacao = ''
-			operacao = ''
-			numero1 = ''
-			numero2 = ''
-			break;
-		case '-':
-			
-			NumAnterior.innerText = ""
-			arrayNumAnterior = ""
-			arrayNumAtual = numero1 - numero2;
-			NumAtual.innerText = arrayNumAtual;
-			arrayTodaOperacao = ''
-			operacao = ''
-			numero1 = ''
-			numero2 = ''
-			break;
-		case '*':
-			NumAnterior.innerText = ""
-			arrayNumAnterior = ""
-			arrayNumAtual = numero1 * numero2;
-			NumAtual.innerText = arrayNumAtual;
-			arrayTodaOperacao = ''
-			operacao = ''
-			numero1 = ''
-			numero2 = ''
-			break;
-		case '/':
-			NumAnterior.innerText = ""
-			arrayNumAnterior = ""
-			arrayNumAtual = numero1 / numero2;
-			NumAtual.innerText = arrayNumAtual;
-			arrayTodaOperacao = ''
-			operacao = ''
-			numero1 = ''
-			numero2 = ''
-			break;		
-		default:
-			break;
+	if((operacao != '') && (arrayNumAtual != '') && (arrayNumAnterior != '')){
+		switch (operacao) {
+			case '+':
+				NumAnterior.innerText = ""
+				arrayNumAnterior = ""
+				arrayNumAtual = numero1 + numero2;
+				NumAtual.innerText = arrayNumAtual;
+				arrayTodaOperacao = ''
+				operacao = ''
+				numero1 = ''
+				numero2 = ''
+				break;
+			case '-':
+				
+				NumAnterior.innerText = ""
+				arrayNumAnterior = ""
+				arrayNumAtual = numero1 - numero2;
+				NumAtual.innerText = arrayNumAtual;
+				arrayTodaOperacao = ''
+				operacao = ''
+				numero1 = ''
+				numero2 = ''
+				break;
+			case '*':
+				NumAnterior.innerText = ""
+				arrayNumAnterior = ""
+				arrayNumAtual = numero1 * numero2;
+				NumAtual.innerText = arrayNumAtual;
+				arrayTodaOperacao = ''
+				operacao = ''
+				numero1 = ''
+				numero2 = ''
+				break;
+			case '/':
+				NumAnterior.innerText = ""
+				arrayNumAnterior = ""
+				arrayNumAtual = numero1 / numero2;
+				NumAtual.innerText = arrayNumAtual;
+				arrayTodaOperacao = ''
+				operacao = ''
+				numero1 = ''
+				numero2 = ''
+				break;		
+			default:
+				break;
+		}
+	}else{
+		return;
 	}
+	
 });
